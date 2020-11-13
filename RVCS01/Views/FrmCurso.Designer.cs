69e197ef6f97954f536b201bd2cd019bb5a59b6d
erik.ssantos <erik.ssantos@ITQ0603145W10-1>
2020-11-13 10:56:55 -0300

    Projeto

---
+++ RVCS01/Views/FrmCurso.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RVCS01
+{
+    partial class FrmCurso
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListar = new System.Windows.Forms.Button();
+            this.lsbCursos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(31, 124);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lsbCursos
+            // 
+            this.lsbCursos.FormattingEnabled = true;
+            this.lsbCursos.Location = new System.Drawing.Point(153, 78);
+            this.lsbCursos.Name = "lsbCursos";
+            this.lsbCursos.Size = new System.Drawing.Size(635, 173);
+            this.lsbCursos.TabIndex = 1;
+            // 
+            // FrmCurso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsbCursos);
+            this.Controls.Add(this.btnListar);
+            this.Name = "FrmCurso";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.ListBox lsbCursos;
+    }
+}
+
---
+++ RVCS01/Views/FrmCurso.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RVCS01.Class;
+
+namespace RVCS01
+{
+    public partial class FrmCurso : Form
+    {
+        public FrmCurso()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lsbCursos.Items.Clear();
+            Curso curso = new Curso();
+            var lista = curso.Listar();
+            foreach (var item in lista)
+                {
+                lsbCursos.Items.Add(item.Id + " - " +item.Nome);
+                }
+        }
+    }
+}
